b7c73740117e5d9814512fb04b602f9a0727e40c
Ethan Gruber <ewg4xuva@gmail.com>
2017-06-15 10:58:45 -0400

    added zoom glyph on hover over iiif image divs

--- ui/javascript/display_functions.js
+++ ui/javascript/display_functions.js
@@ -5,6 +5,15 @@ Library: jQuery
 Description: Rendering graphics based on hoard counts
  ************************************/
 $(document).ready(function () {
+    
+    //display the magnifying glass glyph when hovering the mouse of divs that contain it (for IIIF)
+    $('.g_doc:has(.iiif-zoom-glyph)').hover(function () {
+        $(this).find('.iiif-zoom-glyph').fadeIn();
+    },
+    function () {
+        $(this).find('.iiif-zoom-glyph').fadeOut();
+    });
+    
     $('a.thumbImage').fancybox({
         type: 'image',
         beforeShow: function () {
@@ -21,7 +30,7 @@ $(document).ready(function () {
         beforeShow: function () {
             var manifest = this.element.attr('manifest');
             //remove and replace #iiif-container, if different or new
-            if (manifest != $('#manifest').text()) {          
+            if (manifest != $('#manifest').text()) {
                 $('#iiif-container').remove();
                 $(".iiif-container-template").clone().removeAttr('class').attr('id', 'iiif-container').appendTo("#iiif-window");
                 $('#manifest').text(manifest);
@@ -40,12 +49,12 @@ $(document).ready(function () {
             center:[0, 0],
             crs: L.CRS.Simple,
             zoom: 0
-        });        
+        });
         
         // Grab a IIIF manifest
-        $.getJSON(manifest, function (data) {       
+        $.getJSON(manifest, function (data) {
             //determine where it is a collection or image manifest
-            if (data[ '@context'] == 'http://iiif.io/api/image/2/context.json' || data['@context'] == 'http://library.stanford.edu/iiif/image-api/1.1/context.json') {
+            if (data[ '@context'] == 'http://iiif.io/api/image/2/context.json' || data[ '@context'] == 'http://library.stanford.edu/iiif/image-api/1.1/context.json') {
                 L.tileLayer.iiif(manifest).addTo(iiifImage);
             } else if (data[ '@context'] == 'http://iiif.io/api/presentation/2/context.json') {
                 var iiifLayers = {
